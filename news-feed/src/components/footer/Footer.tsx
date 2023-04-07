b815d6319ed6408bf4932e2e26f397df514daea1
Morozov Dmitry <d.morozov@qk7p.ru>
2023-04-07 18:17:08 +0300

    footer init

--- news-feed/src/components/footer/Footer.tsx
+++ news-feed/src/components/footer/Footer.tsx
@@ -7,7 +7,23 @@ export interface IFooterProps {
 }
 
 const Footer: FC<IFooterProps> = ({ className }) => {
-  return <div className={classNames(styles.footer, className)}></div>;
+  return (
+    <div className={classNames(styles.footer, className)}>
+      <span className={styles.name}>qk7p</span>
+      <span className={styles.label}>посмотреть резюме:</span>
+      <a
+        href={
+          "https://korolev.hh.ru/resume/fdf5bbb7ff08a222580039ed1f484672336d39"
+        }
+        className={styles.link}
+      >
+        hh
+      </a>
+      <a href={"https://qk7p.ru"} className={styles.link} target={"_blank"}>
+        cайт
+      </a>
+    </div>
+  );
 };
 
 export default Footer;
