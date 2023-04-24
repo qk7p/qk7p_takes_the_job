4bb654c961d0b66698e83794dccac1be4eaaf13d
Morozov Dmitry <d.morozov@qk7p.ru>
2023-04-24 18:10:17 +0300

    убрал стрикт

--- news-feed/src/main.tsx
+++ news-feed/src/main.tsx
@@ -7,13 +7,11 @@ import store, { persistor } from "./store/store";
 import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </PersistGate>
-    </Provider>
-  </React.StrictMode>
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </PersistGate>
+  </Provider>
 );
